9851e43c61869aab15798bb48d3817fb972e74a8
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-27 20:49:18 -0500

    First Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'selector',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			screens: {
+				xs: '430px'
+			},
+			fontFamily:{
+				Rubik : ['Rubik Variable', 'sans-serif']
+				},
+		}
+	},
+	plugins: []
+};
