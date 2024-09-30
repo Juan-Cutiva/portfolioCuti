111c62b6fa8b84d241a51aa0ee833ff5edd1ff0d
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-30 18:01:31 -0500

    [BUILD] Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  i18n:{
+    defaultLocale:'es',
+    locales: ['es', 'en']
+  }
 });
