1d11cbd92f6274748a8016e7330fe246a9169380
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-09-30 17:15:52 -0500

    "Added language support to Layout and Hero components"

---
+++ src/i18n/ui.ts
@@ -0,0 +1,16 @@
+export const languages = {
+	es: 'Español',
+	en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+	es: {
+		'nav.home': 'Casa'
+	},
+	en: {
+		'nav.home': 'Home',
+	}
+} as const;
+
